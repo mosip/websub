f4ddce3c931634681aea4cd49f50530c9b2a893a
rambhatt1591 <ram.bhatt@technoforte.co.in>
2021-04-01 14:25:08 +0530

    MOSIP-12468 DB Scripts and data models for websub tables

--- db_scripts/mosip_websub/mosip_websub_ddl_deploy.sql
+++ db_scripts/mosip_websub/mosip_websub_ddl_deploy.sql
@@ -2,3 +2,7 @@
 
 \ir ddl/websub-topic.sql
 \ir ddl/websub-subscription.sql
+
+\ir ddl/websub-message_store.sql
+\ir ddl/websub-message_delivery_success.sql
+\ir ddl/websub-message_delivery_failed.sql
