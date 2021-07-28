7e2ce27922b2e376c7d359865bf499cfc6c69f8b
rambhatt1591 <ram.bhatt@technoforte.co.in>
2021-07-28 12:12:46 +0530

    MOSIP-16202 Size Limit removed on message column

--- db_release_scripts/mosip_websub/sql/1.2_websub-scripts_release.sql
+++ db_release_scripts/mosip_websub/sql/1.2_websub-scripts_release.sql
@@ -1,15 +1,20 @@
 -- -------------------------------------------------------------------------------------------------
 -- Database Name: mosip_websub
--- Release Version 	: 1.1.5
+-- Release Version 	: 1.2
 -- Purpose    		: Database Alter scripts for the release for Websub DB.       
 -- Create By   		: Ram Bhatt
 -- Created Date		: Jul-2021
 -- 
 -- Modified Date        Modified By         Comments / Remarks
 -- -------------------------------------------------------------------------------------------------
+-- Jul-2021		Ram Bhatt	    Size limit removed from message column
+-----------------------------------------------------------------------------------------------------
 
 \c mosip_websub sysadmin
 
+-----------------------------------------------------------------------------------------------------
+ALTER TABLE websub.message_store ALTER COLUMN message TYPE character varying;
+-----------------------------------------------------------------------------------------------------
 
 \ir ../ddl/websub-subscription_history.sql
 
--- db_scripts/mosip_websub/ddl/websub-message_store.sql
+++ db_scripts/mosip_websub/ddl/websub-message_store.sql
@@ -8,14 +8,14 @@
 -- 
 -- Modified Date        Modified By         Comments / Remarks
 -- ------------------------------------------------------------------------------------------
--- 
+-- Jul-2021		Ram Bhatt	    Size limit removed from message column
 -- ------------------------------------------------------------------------------------------
 
 -- object: websub.message_store | type: TABLE --
 -- DROP TABLE IF EXISTS websub.message_store CASCADE;
 CREATE TABLE websub.message_store (
 	id character varying(36) NOT NULL,
-	message character varying(1024) NOT NULL,
+	message character varying NOT NULL,
 	topic character varying(256) NOT NULL,
 	publisher character varying(256),
 	pub_dtimes timestamp NOT NULL,
